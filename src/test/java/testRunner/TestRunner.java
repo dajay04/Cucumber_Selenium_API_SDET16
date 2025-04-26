6c5a7a8c11797ed109f5685f97e359752930ed33
dajay04 <dahiyaajay370@gmail.com>
2025-04-26 08:27:25 +0530

    Config files

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,7 +10,7 @@ import stepDefinitions.BaseClass;
 @RunWith(Cucumber.class)
 @CucumberOptions
         (
-            features = "src/main/resources/Features",
+            features = "src/test/resources/Features",
             glue = "stepDefinitions",
             tags = "@regression",
             monochrome = true,// it will remove unneccsary characters in console window
